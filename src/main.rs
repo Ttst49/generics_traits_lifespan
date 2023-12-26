f6c843e5d4626ce3b615d6c58bc3b8729b44c574
Ttst <thibautstachnick@gmail.com>
2023-12-26 15:18:05 +0100

    add a function in order to see the greater number from a single function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    launch_greater_application()
+}
+
+
+fn launch_greater_application(){
+    let number_list = vec![13,273,283,109,588,637];
+
+    println!("Le plus grand nombre est {}",find_greater_number(&number_list))
+}
+
+fn find_greater_number(list: &[i64])->i64{
+    let mut greater_number = list[0];
+
+    for &number in list {
+        if number > greater_number {
+            greater_number = number
+        }
+    }
+    greater_number
 }
