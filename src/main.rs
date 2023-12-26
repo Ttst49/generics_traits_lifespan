4942134127f925753909f2249b40990a934ff2b5
Ttst <thibautstachnick@gmail.com>
2023-12-26 16:03:57 +0100

    end generic and comment what's compile into an error

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ fn main() {
 fn launch_greater_application(){
     let number_list = vec![13,273,283,109,588,637];
 
-    println!("Le plus grand nombre est {}",find_greater(&number_list));
+    println!("Le plus grand nombre est {}",find_greater_number_i64(&number_list));
 
     let characters_list = vec!['y','x','v','h','d','e'];
 
-    println!("Le plus grand caractère est {}",find_greater(&characters_list));
+    println!("Le plus grand caractère est {}",find_greater_char(&characters_list));
 }
 
 //function that return i64 value
@@ -38,15 +38,51 @@ fn find_greater_char(list : &[char])->char{
 }
 
 //generic function that return the greater value from a T generic variable
-fn find_greater<T>(list: &[T])->T{
+//fn find_greater<T>(list: &[T])->T{
 
-    let mut greater_value = list[0];
+  //  let mut greater_value = list[0];
 
-    for &value in list {
-        if value > greater_value {
-            greater_value = value;
-        }
+    //for &value in list {
+      //  if value > greater_value {
+        //    greater_value = value;
+        //}
+    //}
+    //greater_value
+
+//}
+
+//start generic in struct
+struct Point<T>{
+    x:T,
+    y:T
+}
+//can't use 2 different types with the same generic variable
+
+struct PointWith2Variables<A,B>{
+    x:A,
+    y:B
+}
+
+//using generic in impl
+impl<T> Point<T> {
+    fn x(&self)->&T{
+        &self.x
     }
-    greater_value
+    fn y(&self)->&T{
+        &self.y
+    }
+}
+
+fn create_point_from_generic_struct(){
+    let ints = Point{ x:12, y:89 };
+    let floats = Point{ x:73.34, y:954.5 };
+
+    //example 2 different type with same generic variable
+    //let error = Point{ x: 12, y: 26.2 };
+    //example 2 different type with different generic variable
+    //let good = PointWith2Variables{ x:12, y:16.5 };
+
+    //using generic function from impl
+    println!("int.x ={} et int.y = {}",ints.x(),floats.y())
 
 }
