48efebceb3e83031a0e6dfe7ceb46309ffc493d4
Ttst <thibautstachnick@gmail.com>
2023-12-26 15:33:50 +0100

    add a generic function in order to find greater char from a vec!

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,15 @@ fn main() {
 fn launch_greater_application(){
     let number_list = vec![13,273,283,109,588,637];
 
-    println!("Le plus grand nombre est {}",find_greater_number(&number_list))
+    println!("Le plus grand nombre est {}",find_greater_number_i64(&number_list));
+
+    let characters_list = vec!['y','x','v','h','d','e'];
+
+    println!("Le plus grand caractère est {}",find_greater_char(&characters_list));
 }
 
-fn find_greater_number(list: &[i64])->i64{
+//generic function that return i64 value
+fn find_greater_number_i64(list: &[i64])->i64{
     let mut greater_number = list[0];
 
     for &number in list {
@@ -18,4 +23,16 @@ fn find_greater_number(list: &[i64])->i64{
         }
     }
     greater_number
+}
+
+//generic function that return char value
+fn find_greater_char(list : &[char])->char{
+    let mut greater_char = list[0];
+
+    for &char in list {
+        if char > greater_char{
+            greater_char = char
+        }
+    }
+    greater_char
 }
